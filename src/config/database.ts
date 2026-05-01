b069dad37422e09829b067ab2759c61b9268ffd8
Rafael Musmanno <rafael.musmanno@compuletra.com.br>
2026-05-01 20:31:51 -0300

    fix: fixing database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,17 @@
+import "dotenv/config";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not defined in .env");
+}
+
+const adapter = new PrismaBetterSqlite3({ url: dbUrl });
+
 const prisma = new PrismaClient({
+  adapter,
   log: ['query', 'info', 'warn', 'error'],
 });
 
