e781b2409525294b4c71e0f440fc7fc1c7c9a86d
Rafael Musmanno <rafael.musmanno@compuletra.com.br>
2026-05-01 19:54:03 -0300

    fix: fixing prisma database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,7 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
 
 export default prisma;
